6cb7ec5b798f3dfad18d95801fec10284fbad726
makefu <github@syntax-fehler.de>
2026-01-13 20:59:49 +0100

    flake: update to latest unstable
    refactor the build procedure to support multi-system

--- flake.nix
+++ flake.nix
@@ -5,11 +5,33 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.mediawiki_matrix_bot = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.mediawiki_matrix_bot;
-
-  };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = pkgsFor system;
+        in {
+          mediawiki_matrix_bot = pkgs.python3Packages.buildPythonApplication {
+            pname = "mediawiki-matrix-bot";
+            version = "1.1.0";
+            src = ./.;
+            pyproject = true;
+            build-system = [ pkgs.python3Packages.setuptools ];
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              matrix-nio docopt aiohttp
+            ];
+            nativeBuildInputs = with pkgs.python3Packages; [
+              mypy
+            ];
+            checkPhase = ''
+              mypy --strict mediawiki_matrix_bot
+            '';
+          };
+          default = self.packages.${system}.mediawiki_matrix_bot;
+        });
+    };
 }
