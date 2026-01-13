d50cb0701850dac745aa60c4c8d10f6660a82b8a
makefu <github@syntax-fehler.de>
2026-01-13 22:39:10 +0100

    flake: export overlay

--- flake.nix
+++ flake.nix
@@ -10,27 +10,33 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       pkgsFor = system: nixpkgs.legacyPackages.${system};
+      
+      mkPackage = pkgs: pkgs.python3Packages.buildPythonApplication {
+        pname = "mediawiki-matrix-bot";
+        version = "1.2.0";
+        src = ./.;
+        pyproject = true;
+        build-system = [ pkgs.python3Packages.setuptools ];
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          matrix-nio docopt aiohttp
+        ];
+        nativeBuildInputs = with pkgs.python3Packages; [
+          mypy
+        ];
+        checkPhase = ''
+          mypy --strict mediawiki_matrix_bot
+        '';
+      };
     in {
+      overlays.default = final: prev: {
+        mediawiki-matrix-bot = mkPackage final;
+      };
+
       packages = forAllSystems (system:
         let
           pkgs = pkgsFor system;
         in {
-          mediawiki_matrix_bot = pkgs.python3Packages.buildPythonApplication {
-            pname = "mediawiki-matrix-bot";
-            version = "1.2.0";
-            src = ./.;
-            pyproject = true;
-            build-system = [ pkgs.python3Packages.setuptools ];
-            propagatedBuildInputs = with pkgs.python3Packages; [
-              matrix-nio docopt aiohttp
-            ];
-            nativeBuildInputs = with pkgs.python3Packages; [
-              mypy
-            ];
-            checkPhase = ''
-              mypy --strict mediawiki_matrix_bot
-            '';
-          };
+          mediawiki_matrix_bot = mkPackage pkgs;
           default = self.packages.${system}.mediawiki_matrix_bot;
         });
     };
